b87e40a0fbf9ac5cd066a39aa0f8a1eb77931b71
Julia <granovskaya.97@mail.ru>
2022-07-18 13:07:48 +0300

    Add tasks

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿// Задать двумерный массив размером m*n
-
+/*
 int[,] GenerateRandomArray(int l, int k)
 {
     int[,] array = new int[l, k];
     for (int i = 0; i < l; i++)
     {
-        for (int j = 0; i < k; j++)
+        for (int j = 0; j < k; j++)
         {
             array[i, j] = new Random().Next(-9, 10);
         }
@@ -18,10 +18,10 @@ void ShowArray(int[,] array)
  {
     for (int column = 0; column < array.GetLength(1); column++)
     {
-        Console.Write(array[row, column] +" ");
+        Console.Write(array[row,column] +" ");
     }
- }   
     Console.WriteLine();
+ }
 }
 
 Console.Write("Введите кол-во сток таблицы: ");
@@ -30,3 +30,128 @@ Console.Write("Введите кол-во стобцов таблицы: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[,] myarray = GenerateRandomArray(m, n);
 ShowArray(myarray);
+*/
+// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+/*
+int[,] GenerateRandomArray(int l, int k)
+{
+    int[,] array = new int[l, k];
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+void ShowArray(int[,] array)
+{
+ for (int row = 0; row < array.GetLength(0); row++)
+ {
+    for (int column = 0; column < array.GetLength(1); column++)
+    {
+        Console.Write(array[row,column] +" ");
+    } 
+    Console.WriteLine();
+ }
+}
+
+Console.Write("Введите кол-во сток таблицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во стобцов таблицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] myarray = GenerateRandomArray(m, n);
+ShowArray(myarray);
+*/
+
+// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+/*
+int[,] GenerateRandomArray(int l, int k)
+{
+    int[,] array = new int[l, k];
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            array[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return array;
+}
+void ShowArray(int[,] array)
+{
+ for (int row = 0; row < array.GetLength(0); row++)
+ {
+    for (int column = 0; column < array.GetLength(1); column++)
+    {
+        Console.Write(array[row,column] +" ");
+    } 
+    Console.WriteLine();
+ }
+}
+
+Console.Write("Введите кол-во сток таблицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во стобцов таблицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] myarray = GenerateRandomArray(m, n);
+ShowArray(myarray);
+Console.WriteLine();
+for (int index = 0; index < m; index = index + 2)
+    {
+        for (int count = 0; count < n; count = count + 2)
+        {
+            myarray[index, count] = myarray[index, count] * myarray[index, count];
+        }
+    }
+ShowArray(myarray);
+*/
+
+// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+/*
+int[,] GenerateRandomArray(int l, int k)
+{
+    int[,] array = new int[l, k];
+    for (int i = 0; i < l; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            array[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return array;
+}
+void ShowArray(int[,] array)
+{
+ for (int row = 0; row < array.GetLength(0); row++)
+ {
+    for (int column = 0; column < array.GetLength(1); column++)
+    {
+        Console.Write(array[row,column] +" ");
+    }
+    Console.WriteLine();
+ }
+}
+
+Console.Write("Введите кол-во сток таблицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во стобцов таблицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] myarray = GenerateRandomArray(m, n);
+ShowArray(myarray);
+Console.Write("Сумма элементов главной диагонали: ");
+int sum = 0;
+for (int index = 0; index < m; index++)
+    {
+        for (int count = 0; count < n; count++)
+        {
+            if (index == count)
+            {
+                sum = sum + myarray[index, count];
+                Console.Write($"{myarray[index, count]} +");
+            }
+        }
+    }
+Console.Write($"= {sum}");
+*/
